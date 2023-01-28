913332593c299bd8b4a2f2d7815f73ed522fb058
takahiro.suzaki <taka.58.jps@gmail.com>
2023-01-28 22:33:59 +0900

    消費税計算を整数値に修正

--- src/Review01.java
+++ src/Review01.java
@@ -3,15 +3,15 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500;
-        double result;
+        int result;
         result = tax(price,0.1);
-        int cost = price + (int)result ; 
+        int cost = price + result ; 
         System.out.println(price + "円の商品の税込価格は"+ cost +"円（消費税は"+(int)result+"円）です。");
 
     }
     
-    public static double tax(int p, double t) {
-        double result = p * t;
+    public static int tax(int p, double t) {
+        int result = (int)Math.floor(p * t);
         return result;
     }
 }
